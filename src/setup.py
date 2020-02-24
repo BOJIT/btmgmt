77459ccd66fb9528d66b0450056499344803bab1
James Bennion-Pedley <James.Bennion-Pedley@dyson.com>
2020-02-24 14:26:16 +0000

    port from stash

---
+++ src/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup, Extension
+
+bluez_mgmt = Extension('bluez_mgmt',
+                       sources=['bluez_mgmt.c',
+                                'src/shared/mgmt.c',
+                                'src/shared/util.c',
+                                'src/shared/queue.c',
+                                'src/shared/mainloop.c',
+                                'src/shared/io-mainloop.c',
+                                'src/shared/timeout-mainloop.c',
+                                'src/shared/mainloop-notify.c',
+                                'src/shared/shell.c',
+                                'src/shared/log.c',
+                                'src/uuid-helper.c'],
+                       include_dirs=['/usr/include/python3.7/',
+                                     './'],
+                       libraries=["bluetooth", "readline"])
+
+setup(name = 'bluez_mgmt',
+      version = '1.0',
+      description ='A simplified Python module that uses the bluetooth \
+         management API to reconfigure bluetooth adapter properties: \
+         see https://git.kernel.org/pub/scm/bluetooth/bluez.git/tree/doc/mgmt-api.txt',
+      author = 'James B-P',
+      ext_modules = [bluez_mgmt])
