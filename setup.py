ff7c898b1b72b5ac2250a53bab5546c5ae9a5ff8
BOJIT <35117353+BOJIT@users.noreply.github.com>
2020-05-18 10:06:38 +0100

    convert to test repo for PyPI

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
-#!/usr/bin/env python3
+import setuptools
 
-from distutils.core import setup, Extension
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-bluez_mgmt = Extension('bluez_mgmt',
-                       sources=['bluez_mgmt.c',
-                                'src/shared/mgmt.c',
-                                'src/shared/util.c',
-                                'src/shared/queue.c',
-                                'src/shared/mainloop.c',
-                                'src/shared/io-mainloop.c',
-                                'src/shared/timeout-mainloop.c',
-                                'src/shared/mainloop-notify.c',
-                                'src/shared/shell.c',
-                                'src/shared/log.c',
-                                'src/uuid-helper.c'],
-                       include_dirs=['./'],
-                       libraries=["bluetooth", "readline"])
-
-setup(name = 'bluez_mgmt',
-      version = '1.0',
-      description ='A simplified Python module that uses the bluetooth \
-         management API to reconfigure bluetooth adapter properties: \
-         see https://git.kernel.org/pub/scm/bluetooth/bluez.git/tree/doc/mgmt-api.txt',
-      author = 'James B-P',
-      ext_modules = [bluez_mgmt])
+setuptools.setup(
+    name="btmgmt",
+    version="0.0.1",
+    author="James Bennion-Pedley",
+    author_email="jamesbpjames@gmail.com",
+    description="TODO",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BOJIT/btmgmt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
